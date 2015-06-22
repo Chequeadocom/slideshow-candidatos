de7755dc186076ad69dd9ed28b351b6e23ff46cf
Pablo H. Paladino <paladinop@gmail.com>
2015-06-22 19:16:39 -0300

    base

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var Chequeado;
+
+;(function(global, document, $){
+
+    "use strict";
+
+    Chequeado = global.Chequeado = global.Chequeado || {};
+
+    Chequeado.init = function(){
+        console.log('init!');
+    };
+
+})(window, document,jQuery);
